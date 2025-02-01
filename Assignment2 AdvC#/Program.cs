0c28c0ce47e328377ca5564eb94743f47c3fa5f2
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-02-01 13:06:07 +0200

    Q1

--- Assignment2 AdvC#/Program.cs
+++ Assignment2 AdvC#/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Assignment2_AdvC_
+﻿using System.Collections;
+
+namespace Assignment2_AdvC_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 : You are given an ArrayList containing a sequence of elements. try to reverse the order of elements in the ArrayList in-place(in the same arrayList) without using the built-in Reverse. Implement a function that takes the ArrayList as input and modifies it to have the reversed order of elements.
+
+            ArrayList nums = new ArrayList() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            ArrayList reversed = Reverse(nums);
+
+            foreach (int n in reversed)
+            {
+                Console.WriteLine(n);
+            }
+
+            #endregion
+        
+        
         }
+
+        #region Q1 : You are given an ArrayList containing a sequence of elements. try to reverse the order of elements in the ArrayList in-place(in the same arrayList) without using the built-in Reverse. Implement a function that takes the ArrayList as input and modifies it to have the reversed order of elements.
+        public static ArrayList Reverse(ArrayList list)
+        {
+            ArrayList reversedList = new ArrayList();
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                reversedList.Add(list[i]);
+            }
+            return reversedList;
+        } 
+        #endregion
+
     }
 }
