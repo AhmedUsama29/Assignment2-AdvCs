6591bb6e1d6c6a8040b7381a830c885335d8c766
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-02-01 12:59:53 +0200

    Add project files.

---
+++ Assignment2 AdvC#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment2_AdvC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
